aa8d46c24de82bb9aca9a49a7094d30390f2b0b4
michaelliberatore <m.liberatore98@gmail.com>
2020-09-13 17:18:18 +1000

    Imagenet update

---
+++ click/assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+	console.log('hello world');
+
+	$('.btn-box').click(function(){
+		$('.box').toggleClass('active');
+	});
+
+	$('.box').click(function(){
+		$(this).toggleClass('active');
+	});
+
+	$('.btn-slide').click(function(){
+		$('.slide').toggleClass('active');
+	});
+
+
+	$('.btn-alert').click(function(){
+		$('alert').toggleClass('active');
+	});
+
+	$('.btn-mag').click(function(){
+		$('.item').addClass('hide');
+		$('.item.mag').removeClass('hide')
+	});
+
+	$('.btn-red').click(function(){
+		$('.item').addClass('hide');
+		$('.item.red').removeClass('hide')
+	});
+
+
+});
