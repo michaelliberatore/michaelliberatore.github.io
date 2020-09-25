791a4b6905f2e50b043ca8242b9fa3735d0d7d8b
michaelliberatore <m.liberatore98@gmail.com>
2020-09-25 16:46:22 +1000

    Folio update

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	
+
+	$('.btn-box').click(function(){
+		$('.box').toggleClass('active');
+	});
+
+	$('.box').click(function(){
+		$(this).toggleClass('active');
+	});
+
+	$('.btn-slide').click(function(){
+		$('.slide').toggleClass('active');
+	});
+
+
+	$('.btn-alert').click(function(){
+		$('alert').toggleClass('active');
+	});
+
+	$('.btn-Print').click(function(){
+		$('.item').addClass('hide');
+		$('.item.Print').removeClass('hide')
+	});
+
+	$('.btn-Personal').click(function(){
+		$('.item').addClass('hide');
+		$('.item.Personal').removeClass('hide')
+	});
+
+	});
