e907c9a97c016d36ad0b5d87324b39083b12cc5f
michaelliberatore <m.liberatore98@gmail.com>
2020-10-28 18:16:49 +1100

    Xyz-poster-3 update

---
+++ xyz-poster-5/assets/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+var word1 = [
+	"worm",
+	"rabbit",
+	"tiger",
+  "panda",
+  "cat",
+  "rat"
+	
+];
+
+var randomWord1 = word1[Math.floor(Math.random()*word1.length)];
+
+var word2 = [
+  "grass",
+  "cake",
+  "rice",
+  "dirt",
+  "fish"
+];
+
+var randomWord2 = word2[Math.floor(Math.random()*word2.length)];
+
+var word3 = [
+  "legs",
+  "arms",
+  "eyes",
+  "hair",
+  "teeth"
+];
+
+var randomWord3 = word3[Math.floor(Math.random()*word3.length)];
+
+$( ".word1" ).html(randomWord1);
+$( ".word2" ).html(randomWord2);
+$( ".word3" ).html(randomWord3);
+
+});
