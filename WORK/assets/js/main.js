1334396ebf2a0ad7352949655d1f23c3c7bba323
michaelliberatore <m.liberatore98@gmail.com>
2026-05-26 15:20:32 +1000

    Update index.html

--- WORK/assets/js/main.js
+++ WORK/assets/js/main.js
@@ -1,39 +1,81 @@
 $(document).ready(function(){
-	
 
-	$('.btn-box').click(function(){
-		$('.box').toggleClass('active');
-	});
+/* =========================
+   UI TOGGLES
+========================= */
 
-	$('.box').click(function(){
-		$(this).toggleClass('active');
-	});
+$('.btn-box').click(function(){
+$('.box').toggleClass('active');
+});
 
-	$('.btn-slide').click(function(){
-		$('.slide').toggleClass('active');
-	});
+$('.box').click(function(){
+$(this).toggleClass('active');
+});
 
-	$('.btn-alert').click(function(){
-		$('alert').toggleClass('active');
-	});
+$('.btn-slide').click(function(){
+$('.slide').toggleClass('active');
+});
 
-	$('.btn-Print').click(function(){
-		$('.item').addClass('hide');
-		$('.item.Print').removeClass('hide')
-	});
+$('.btn-alert').click(function(){
+$('alert').toggleClass('active');
+});
 
-	$('.btn-Photography').click(function(){
-		$('.item').addClass('hide');
-		$('.item.Photography').removeClass('hide')
-	});
+/* =========================
+   FILTER SYSTEM (WORK PAGE)
+========================= */
 
-	$('.btn-Client').click(function(){
-		$('.item').addClass('hide');
-		$('.item.Client').removeClass('hide')
-	});
+$('.btn-Print').click(function(){
+$('.item').addClass('hide');
+$('.item.Print').removeClass('hide');
+});
 
-	$('.btn-reset').click(function(){
-	    $('.item').removeClass('hide');
-	});
+$('.btn-Photography').click(function(){
+$('.item').addClass('hide');
+$('.item.Photography').removeClass('hide');
+});
 
-	});
+$('.btn-Client').click(function(){
+$('.item').addClass('hide');
+$('.item.Client').removeClass('hide');
+});
+
+$('.btn-reset').click(function(){
+$('.item').removeClass('hide');
+});
+
+/* =========================
+   CAROUSEL (ARROWS + AUTO SLIDE)
+========================= */
+
+$('.carousel').each(function(){
+
+let $carousel = $(this);
+let $track = $carousel.find('.carousel-track');
+let $slides = $carousel.find('img');
+let index = 0;
+
+function updateCarousel(){
+$track.css('transform', 'translateX(' + (-index * 100) + '%)');
+}
+
+/* NEXT */
+$carousel.find('.next').click(function(){
+index = (index + 1) % $slides.length;
+updateCarousel();
+});
+
+/* PREVIOUS */
+$carousel.find('.prev').click(function(){
+index = (index - 1 + $slides.length) % $slides.length;
+updateCarousel();
+});
+
+/* AUTO PLAY */
+setInterval(function(){
+index = (index + 1) % $slides.length;
+updateCarousel();
+}, 3500);
+
+});
+
+});
