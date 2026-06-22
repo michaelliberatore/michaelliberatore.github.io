3d03fd10b436e17a255d26564fa16f15c30de717
michaelliberatore <m.liberatore98@gmail.com>
2026-06-22 16:08:27 +1000

    Update index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,79 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$('.btn-box').click(function(){
-$('.box').toggleClass('active');
+  // =========================
+  // TESTIMONIAL CAROUSEL
+  // =========================
+
+  const testimonialTrack = document.querySelector('.testimonial-track');
+  const testimonialSlides = document.querySelectorAll('.testimonial');
+  const testimonialPrev = document.querySelector('.testimonial-carousel .prev');
+  const testimonialNext = document.querySelector('.testimonial-carousel .next');
+
+  if (testimonialTrack && testimonialSlides.length && testimonialNext && testimonialPrev) {
+
+    let testimonialIndex = 0;
+
+    function updateTestimonials() {
+      testimonialTrack.style.transform =
+        `translateX(-${testimonialIndex * 100}%)`;
+    }
+
+    testimonialNext.addEventListener('click', () => {
+      testimonialIndex =
+        (testimonialIndex + 1) % testimonialSlides.length;
+      updateTestimonials();
+    });
+
+    testimonialPrev.addEventListener('click', () => {
+      testimonialIndex =
+        (testimonialIndex - 1 + testimonialSlides.length) %
+        testimonialSlides.length;
+      updateTestimonials();
+    });
+  }
+
+  // =========================
+  // UI TOGGLES
+  // =========================
+
+  $('.btn-box').click(function () {
+    $('.box').toggleClass('active');
+  });
+
+  $('.box').click(function () {
+    $(this).toggleClass('active');
+  });
+
+  $('.btn-slide').click(function () {
+    $('.slide').toggleClass('active');
+  });
+
+  // FIXED (you had invalid selector before)
+  $('.btn-alert').click(function () {
+    $('.alert').toggleClass('active');
+  });
+
+  // =========================
+  // FILTER SYSTEM
+  // =========================
+
+  $('.btn-Print').click(function () {
+    $('.item').addClass('hide');
+    $('.item.Print').removeClass('hide');
+  });
+
+  $('.btn-Photography').click(function () {
+    $('.item').addClass('hide');
+    $('.item.Photography').removeClass('hide');
+  });
+
+  $('.btn-Client').click(function () {
+    $('.item').addClass('hide');
+    $('.item.Client').removeClass('hide');
+  });
+
+  $('.btn-reset').click(function () {
+    $('.item').removeClass('hide');
+  });
+
 });
-
-$('.box').click(function(){
-$(this).toggleClass('active');
-});
-
-$('.btn-slide').click(function(){
-$('.slide').toggleClass('active');
-});
-
-$('.btn-alert').click(function(){
-$('alert').toggleClass('active');
-});
-
-$('.btn-Print').click(function(){
-$('.item').addClass('hide');
-$('.item.Print').removeClass('hide')
-});
-
-$('.btn-Photography').click(function(){
-$('.item').addClass('hide');
-$('.item.Photography').removeClass('hide')
-});
-
-$('.btn-Client').click(function(){
-$('.item').addClass('hide');
-$('.item.Client').removeClass('hide')
-});
-
-$('.btn-reset').click(function(){
-$('.item').removeClass('hide');
-});
-
